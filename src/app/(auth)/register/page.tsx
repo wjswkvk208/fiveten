e5764f9e2a921c887e5dd99908cf7298ee2ac439
wjswkvk208 <gory0977@gmail.com>
2023-11-29 07:21:35 +0900

    테마 워닝 문제

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Avatar, Box, Button, Checkbox, Container, CssBaseline, FormControlLabel, Grid, TextField, ThemeProvider, Typography, Link, createTheme, Alert } from "@mui/material";
+import { Avatar, Box, Button, Container, CssBaseline, Grid, TextField, Typography, Link, Alert } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Copyright from "@/components/Copyright";
 import { useRegister } from "@/hooks/Auth";
@@ -8,7 +8,6 @@ import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-const defaultTheme = createTheme();
 export default function Register() {
   const router = useRouter();
   const { data, trigger, error, reset } = useRegister();
@@ -50,55 +49,53 @@ export default function Register() {
   }, [data, error, reset]);
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            회원가입
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField required fullWidth id="email" label="이메일" name="email" autoComplete="email" />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField required fullWidth id="username" label="닉네임" name="username" autoComplete="user-name" />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField required fullWidth name="password" label="비밀번호" type="password" id="password" autoComplete="new-password" />
-              </Grid>
-              {error && (
-                <Grid item xs={12}>
-                  <Alert severity="error">{error.message}</Alert>
-                </Grid>
-              )}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          회원가입
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField required fullWidth id="email" label="이메일" name="email" autoComplete="email" />
             </Grid>
+            <Grid item xs={12}>
+              <TextField required fullWidth id="username" label="닉네임" name="username" autoComplete="user-name" />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField required fullWidth name="password" label="비밀번호" type="password" id="password" autoComplete="new-password" />
+            </Grid>
+            {error && (
+              <Grid item xs={12}>
+                <Alert severity="error">{error.message}</Alert>
+              </Grid>
+            )}
+          </Grid>
 
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              회원가입
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  이미 회원이신가요?
-                </Link>
-              </Grid>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            회원가입
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                이미 회원이신가요?
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Copyright sx={{ mt: 5 }} />
+    </Container>
   );
 }
